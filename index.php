8867fcd377944258b44ccd6f26fb0e6b3df1bb5d
CuriousCat318 <sharonleo318@gmail.com>
2017-11-10 15:52:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$con = @mysqli_connect('localhost', 'root', '', 'testdb');
+$con = @mysqli_connect('localhost', 'root', '', 'restaurant');
 if(!$con){
   echo "Error:".mysqli_connect_error();
   exit();
