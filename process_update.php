c7bdf85045795958375135d1806c1e2c87edf4c9
rheasanjan <rheasanjan@gmail.com>
2017-11-18 19:37:13 +0530

    Add files via upload

--- process_update.php
+++ process_update.php
@@ -1,53 +1,25 @@
 <?php
-require('index.php');
-$empid = null;
-if(isset($_POST['empid'])){
-  global $empid;
-  $empid = $_POST['empid'];
-}
-$fname = null;
-$lname = null;
-$phno = null;
-$dob = null;
-$address = null;
-$email = null;
-$salary = null;
-$result = mysqli_query($con, "SELECT * FROM employee WHERE empid = '$empid'");
-if(!$result){
-  printf("Error: %s\n", mysqli_error($con));
-   exit();
-}
-while($row = mysqli_fetch_array($result)){
-  global $fname, $lname, $phno, $dob, $address, $email, $salary;
-  $fname = $row['fname'];
-  $lname = $row['lname'];
-  $phno = $row['phno'];
-  $dob = $row['dob'];
-  $address = $row['address'];
-  $email = $row['email'];
-  $salary = $row['salary'];
-}
+include 'index.php';
+session_start();
+$stmt = $con->prepare("UPDATE employee set fname = (?), lname = (?) , phno= (?) ,address = (?) ,email =(?), salary = (?) where empid= (?)");
+$stmt->bind_param("sssssii", $fname,$lname,$phno,$addr,$email,$sal,$emp);
 
+$fname = $_POST['Fname'];
+$lname = $_POST['Lname'];
+$phno = $_POST['phone'];
+$addr  =$_POST['City'];
+ $emp = $_POST['id'];
+ $sal = $_POST['sal'];
+ $email = $_POST['email'];
 
-
-$sql = "UPDATE employee SET fname = ?, lname = ?, phno = ?, dob = ?, address = ?, email = ?, salary = ? WHERE empid = ?";
-$stmt = $db_usag->prepare($sql);
-
-$stmt->bind_param('ssssssds', $fname, $lname, $phno, $dob, $address, $email, $salary, $empid);
-$fname = $_POST['Fname'] ?: '';
-$lname = $_POST['Lname'] ?: '';
-$phno = $_POST['phone'] ?: '';
-$dob = $_POST['dob'] ?: '';
-$address = $_POST['address'] ?: '';
-$email = $_POST['email'] ?: '';
-$salary = $_POST['sal'] ?: '';
-
-$stmt->execute();
-if ($stmt->errno) {
-  echo "FAILURE!".$stmt->error;
-}
-
-
-
-
-?>
+ if(!$stmt-> execute())
+ {
+   $_SESSION['msg'] = "set";
+   echo $stmt->error();
+ }
+ else {
+   $_SESSION['suc'] ="set";
+   header("location:update_emp.php");
+ }
+ $stmt->close();
+ ?>
--- update_emp.php
+++ update_emp.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <html>
 <head>
   <title>
@@ -19,6 +22,7 @@
       border: 2px solid #ccc;
       border-radius: 4px;
       box-sizing: border-box;
+
     }
     input:focus{
        border: 3px solid #555;
@@ -50,23 +54,22 @@
     input[type=reset]:hover {
         background-color:#57BEEB;
     }
-    div {
-
-        padding: 10px 100px 0 100px;;
-    	width:90%;
-    	height:750px;
-    	font-family:aladin;
-    	color:white;
-    	opacity:0.9;
-    }
-    div.emp {
-      width: 25%;
+div {
+  color: white;
+  font-size: 20px;
+  font-family: cursive;
+}
+    div.empl {
+      padding: 75px 100px;
+      width: 35%;
       height: 750px;
       float: left;
+
     }
     div.remaining{
       width: 500px;
       height:750px;
+
 
     }
     .button {
@@ -99,59 +102,124 @@
   }
 
     }
-    p.eid{
-      float:left;
-      color: beige;
-      font-size: 20px;
-      padding: 5px 5px 5px 5px;
-      font-family:sans-serif;
-    }
+
 
     </style>
   </head>
   <body background="vegetables.jpg">
-    <div>
+
       <center><p style="font-size:40px;color:beige"> <b>Update Employee Details </b> </p>
+      </center>
+        <?php
+        include 'index.php';
+          if (isset($_POST['submit'])) {
+            $emp = $_POST['empid'];
+
+            //echo $emp;
+          }
+          
+          $sql = "SELECT fname,lname,phno,address,email,salary,empid FROM employee where empid = '$emp'" ;
+          $result = mysqli_query($con,$sql);
+          $row = mysqli_fetch_row($result);
+        ?>
 
 
-      <div class="emp">
-        <form action = "update_emp.php" method = "post" name = "getempdetails">
-      <p class="eid"><b> Employee ID </b> <input type="text" name="empid" placeholder="Employee ID" required>
-        <button class="new" id = "getdetails" type = "submit"> Get Employee Details </button>
-      </form>
-      </div>
-      <div class="remaining">
-          <form action="update_emp.php" method="post" name="update" >
-    <p class="name"> <b> First name&nbsp&nbsp&nbsp&nbsp &nbsp</b><input type="text" name="Fname" placeholder="first name" required value = "<?=$fname?>"></p>
-
-     <p class="name"><b>Last name&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp</b><input type="text" name="Lname" placeholder="Last name" required value = "<?=$lname?>"></p>
-     <p class="name"><b>Phone number</b><input type="tel" name="phone" placeholder="Phone no." required value = "<?=$phno?>"></p>
-
-     <p class="name"><b>Date of birth &nbsp&nbsp&nbsp</b><input type="date" name="dob" placeholder="dd/mm/yyyy" required value = "<?=$dob?>"></p>
-     <p class="name"><b>Address &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b><input type="text" name="address" placeholder="address" required value = "<?=$address?>"></p>
-     <p class="name"><b>Email&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp </b> <input type="email" name="email" placeholder="email" required value = "<?=$email?>"></p>
-     <p class="name"><b> Salary &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b><input type="text" name="sal" placeholder="Salary" required value = "<?=$salary?>"></p>
+      <div class="empl">
+    <form action=" " method="post" name="update" >
 
 
+      <label> Employee ID </label> <input type="text" name="empid" placeholder="Employee ID" required> </p>
+        <input type="submit" name ="submit" value="Search employee">
+
+
+    </form>
+    </div>
+<br>
+<br>
+<br>
+<center>
+  <div class="remaining">
+    <center>
+    <form action="process_update.php " method = "post" name="update">
+
+        <?php
+        print "<label> Employee ID</label> <br> <input type=\"text\" name=\"id\" placeholder=\"id\" value=$row[6]  > <br> <br>";
+
+     print "<label> First name</label> <br> <input type=\"text\" name=\"Fname\" placeholder=\"Firstname\" value=$row[0]  > <br> <br>";
+
+     print "<label> Last name </label> <br> <input type=text name=\"Lname\" placeholder=\"Lastname\" value=$row[1] > <br> <br>";
+     print "<label> Phone number</label> <br> <input type=tel name=\"phone\" placeholder=\"Phone no.\" value= $row[2] > <br> <br>";
+
+
+      print "<label>Address </label> <br> <input type=text name=\"City\" placeholder=\"Address\" value=$row[3] > <br><br>";
+      print "<label> Email </label> <br> <input type=email name=\"email\" placeholder=\"email\" value=$row[4] ><br> <br>";
+      print "<label> Salary </label> <br> <input type=text name=\"sal\" placeholder=\"Salary\" value=$row[5] > <br><br>";
+
+?>
 
        <input type="submit" value="Update Details" >
+
        <br>
        <input type="reset" value="Reset">
+     </form>
        <button id="back" class="button"> Back </button>
        <script type="text/javascript">
      document.getElementById("back").onclick = function () {
          location.href = "adminmenu.html";
      };
-     
  </script>
        </center>
      </p>
 
 
 
+</form>
 </div>
-     </form>
-   </div>
+
+
+
  </center>
+ <div id=suc>
+ <?php
+ if(!empty($_SESSION['suc']))
+ {
+   echo '<script language="javascript">';
+   echo 'alert("Update Successful.")';
+
+   echo '</script>';
+
+
+ }
+ ?>
+ </div>
+ <?php unset($_SESSION['suc']);
+ ?>
+ <div id=msg>
+ <?php
+ if(!empty($_SESSION['msg']))
+ {
+   echo '<script language="javascript">';
+   echo 'alert("Cannot be updated")';
+   echo '</script>';
+
+ }
+ ?>
+ </div>
+ <?php unset($_SESSION['msg']);
+ ?>
+ <div id=msg>
+ <?php
+ if(!empty($_SESSION['emp']))
+ {
+   echo '<script language="javascript">';
+   echo 'alert("Employee does not exist")';
+   echo '</script>';
+
+ }
+ ?>
+ </div>
+ <?php unset($_SESSION['emp']);
+ ?>
+
 </body>
 </html>
